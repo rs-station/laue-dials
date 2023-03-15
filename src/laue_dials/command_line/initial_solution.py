24fd81fd488795f69df1a897a26e8afaa5fcdcef
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-03-15 14:45:45 -0400

    Testing entry point + unit test coverage of new scripts

---
+++ src/laue_dials/command_line/initial_solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Tools for generating a monochromatic initial solution to feed into the pipeline.
+
+Examples
+--------
+
+Usage Details
+-------------
+"""
+
+
+def dials_version():
+    """
+    Placeholder function to ensure DIALS is correctly installed and accessible by laue-dials.
+
+    Returns:
+        bool: The return value. True for success, False otherwise.
+    """
+    import reciprocalspaceship as rs
+    from dials.command_line.version import run
+
+    try:
+        run()
+    except:
+        return False
+    return True
---
+++ tests/test_initial_solution.py
@@ -0,0 +1,12 @@
+import pytest
+
+from laue_dials.command_line.initial_solution import dials_version
+
+__author__ = "PrinceWalnut"
+__copyright__ = "PrinceWalnut"
+__license__ = "MIT"
+
+
+def test_dials_version():
+    """API Tests"""
+    assert dials_version() == True
