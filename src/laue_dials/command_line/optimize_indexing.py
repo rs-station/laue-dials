e9c9620e0a014788432814826938a1f87948d5cd
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-04-12 01:32:06 -0400

    Added PHIL arguments and implemented script for optimizing millers/wavelengths jointly

--- src/laue_dials/command_line/optimize_indexing.py
+++ src/laue_dials/command_line/optimize_indexing.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 """
-SHORT_DESCRIPTION
-
-Examples
---------
-
-Usage Details
--------------
+This script optimizes Miller indices and wavelengths jointly.
 """
 
 import logging
 
 import libtbx.phil
 from dials.util import log, show_mail_handle_errors
-from dials.util.options import ArgumentParser
+from dials.util.options import ArgumentParser, reflections_and_experiments_from_files
 
 logger = logging.getLogger("laue-dials.command_line.optimize_indexing")
 
@@ -51,6 +45,27 @@ output {
   log = 'laue.optimize_indexing.log'
     .type = str
     .help = "The log filename."
+  }
+
+n_macrocycles = 3
+  .type = int(value_min=1)
+  .help = "Number of macrocycles of index optimization to perform"
+
+wavelengths {
+  lam_min = 0.95
+    .type = float(value_min=0.1)
+    .help = "Minimum wavelength for beam spectrum"
+  lam_max = 1.15
+    .type = float
+    .help = "Maximum wavelength for beam spectrum"
+  }
+
+reciprocal_grid {
+  d_min = 1.4
+    .type = float(value_min=0.1)
+    .help = "Minimum d-spacing for reflecting planes"
+  }
+
 """,
     process_includes=True,
 )
@@ -58,8 +73,102 @@ output {
 working_phil = phil_scope.fetch(sources=[phil_scope])
 
 
-def index_image(expts, refls):
-    print("To be implemented.")
+def index_image(params, refls, expts):
+    #
+    from laue_dials.algorithms.laue import LaueAssigner
+    from tqdm import tqdm, trange
+    import numpy as np
+    import gemmi
+    from dials.array_family import flex
+    from cctbx.sgtbx import space_group
+    from cctbx.uctbx import unit_cell
+    from dxtbx.model import ExperimentList
+
+    if type(expts) != ExperimentList:
+        expts = ExperimentList([expts])
+
+    # This will populate refls['s1'] & refls['rlp']
+    refls.centroid_px_to_mm(expts)
+    refls.map_centroids_to_reciprocal_space(expts)
+
+    s0 = np.array(expts[0].beam.get_s0())
+
+    # Write to reflection file
+    refls["Wavelength"] = flex.double(len(refls))
+    refls["miller_index"] = flex.miller_index(len(refls))
+
+    for i in trange(len(expts.imagesets())):
+        # Get experiment data from experiment objects
+        experiment = expts[i]
+        cryst = experiment.crystal
+        spacegroup = gemmi.SpaceGroup(
+            cryst.get_space_group().type().universal_hermann_mauguin_symbol()
+        )
+
+        # Get reflections on this image
+        idx = refls["id"] == i
+        subrefls = refls.select(idx)
+
+        # Get unit cell params
+        cell_params = cryst.get_unit_cell().parameters()
+        cell = gemmi.UnitCell(*cell_params)
+
+        # Generate s vectors
+        s1 = subrefls["s1"].as_numpy_array()
+
+        # Get U matrix
+        U = np.asarray(cryst.get_U()).reshape(3, 3)
+
+        # Generate assigner object
+        la = LaueAssigner(
+            s0,
+            s1,
+            cell,
+            U,
+            params.wavelengths.lam_min,
+            params.wavelengths.lam_max,
+            params.reciprocal_grid.d_min,
+            spacegroup,
+        )
+
+        # Optimize Miller indices
+        la.assign()
+        for j in range(params.n_macrocycles):
+            la.reset_inliers()
+            la.update_rotation()
+            la.assign()
+            la.reject_outliers()
+            la.update_rotation()
+            la.assign()
+
+        # Update s1 based on new wavelengths
+        s1[la._inliers] = la.s1 / la.wav[:, None]
+
+        # Reset crystal parameters based on new geometry
+        cryst.set_U(la.R.flatten())
+        cryst.set_A(la.RB.flatten())
+        cryst.set_B(la.B.flatten())
+        cryst.set_space_group(space_group(la.spacegroup.hall))
+        cryst.set_unit_cell(unit_cell(la.cell.parameters))
+
+        # Write data to reflections
+        refls["harmonics"] = flex.bool([False] * len(refls))
+        refls["s1"].set_selected(idx, flex.vec3_double(s1))
+        refls["miller_index"].set_selected(
+            idx,
+            flex.miller_index(la._H.astype("int").tolist()),
+        )
+        refls["Wavelength"].set_selected(
+            idx,
+            flex.double(la._wav.tolist()),
+        )
+        refls["harmonics"].set_selected(
+            idx,
+            flex.bool(la._harmonics.tolist()),
+        )
+
+    # Return reindexed expts, refls
+    return expts, refls
 
 
 @show_mail_handle_errors()
@@ -98,15 +207,21 @@ def run(args=None, *, phil=working_phil, return_results=False):
         parser.print_help()
         return
 
-    # TODO: Optimize indexing routine here
+    # Loop over input files
+    for i in range(len(experiments)):
+        # Reindex data
+        print(f"Reindexing experiment {i}.")
+        indexed_experiments, indexed_reflections = index_image(
+            params, reflections[i], experiments[i]
+        )
 
-    # Save experiments
-    logger.info("Saving optimized experiments to %s", params.output.experiments)
-    indexed_experiments.as_file(params.output.experiments)
+        # Save experiments
+        logger.info("Saving optimized experiments to %s", params.output.experiments)
+        indexed_experiments.as_file(params.output.experiments)
 
-    # Save reflections
-    logger.info("Saving optimized reflections to %s", params.output.reflections)
-    indexed_reflections.as_file(filename=params.output.reflections)
+        # Save reflections
+        logger.info("Saving optimized reflections to %s", params.output.reflections)
+        indexed_reflections.as_file(filename=params.output.reflections)
 
 
 if __name__ == "__main__":
