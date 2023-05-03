8dfa6f71b70b45929848f940519030332422d083
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-05-03 10:43:49 -0400

    Removed no longer existing function's test

--- tests/test_initial_solution.py
+++ tests/test_initial_solution.py
@@ -3,8 +3,3 @@ from laue_dials.command_line.initial_solution import dials_version
 __author__ = "PrinceWalnut"
 __copyright__ = "PrinceWalnut"
 __license__ = "MIT"
-
-
-def test_dials_version():
-    """API Tests"""
-    assert dials_version() == True
