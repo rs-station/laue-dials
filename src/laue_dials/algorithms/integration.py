f5cde1437a215380d1f77071be7c5ad2709a906a
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-05-23 17:21:37 -0400

    Added integration script

---
+++ src/laue_dials/algorithms/integration.py
@@ -0,0 +1,250 @@
+"""
+This file contains useful classes and functions for profiling and integration
+"""
+
+import pandas as pd
+import numpy as np
+import reciprocalspaceship as rs
+from scipy.spatial import KDTree
+
+
+class Profile:
+    def __init__(
+        self,
+        x,
+        y,
+        counts,
+        cen_x=None,
+        cen_y=None,
+        fg_cutoff=1.0,
+        bg_cutoff=3.0,
+        minfrac=0.10,
+        eps=1e-5,
+        frac_step_size=0.50,
+    ):
+        self.frac_step_size = frac_step_size
+        self.eps = eps
+        self.minfrac = minfrac
+        self.fg_cutoff = fg_cutoff
+        self.bg_cutoff = bg_cutoff
+        self.counts = counts
+        self.fg_mask = np.ones_like(counts, dtype=bool)
+        self.bg_mask = np.ones_like(counts, dtype=bool)
+
+        self.x = x
+        self.y = y
+        self.cen_x = cen_x
+        if self.cen_x is None:
+            self.cen_x = np.average(self.x, weights=self.counts)
+        self.cen_y = cen_y
+        if self.cen_y is None:
+            self.cen_y = np.average(self.y, weights=self.counts)
+
+        self.scale = np.cov(self.difference_vectors.T, aweights=counts)
+        self.slope = np.zeros(2)
+        self.intercept = 0.0
+        self.update_mask()
+
+    def set_profile_params(self, scale, slope, intercept, cen_x, cen_y):
+        self.scale = scale
+        self.slope = slope
+        self.intercept = intercept
+        self.cen_x = cen_x
+        self.cen_y = cen_y
+
+        self.update_mask()
+        self.update_background_plane()
+        self.integrate()
+        return self
+
+    def fit(self, nsteps=20):
+        self.success = True
+        for i in range(nsteps):
+            try:
+                self.update_background_plane(alpha=self.frac_step_size)
+                self.update_profile(alpha=self.frac_step_size)
+                self.update_mask()
+            except np.linalg.LinAlgError:
+                self.success = False
+                break
+
+    @classmethod
+    def from_dataframe(cls, df, x_key="dx", y_key="dy", count_key="counts", **kwargs):
+        x = df[x_key].astype("float32")
+        y = df[y_key].astype("float32")
+        counts = df[count_key].astype("float32")
+        return cls(x, y, counts, **kwargs)
+
+    @property
+    def difference_vectors(self):
+        return np.column_stack((self.x - self.cen_x, self.y - self.cen_y))
+
+    @property
+    def background(self):
+        return self.difference_vectors @ self.slope + self.intercept
+
+    def update_mask(self, fg_cutoff=None, bg_cutoff=None):
+        if fg_cutoff is None:
+            fg_cutoff = self.fg_cutoff
+        if bg_cutoff is None:
+            bg_cutoff = self.bg_cutoff
+
+        dx = self.difference_vectors
+        try:
+            Z = np.sqrt(
+                np.squeeze(
+                    dx[..., None, :] @ np.linalg.pinv(self.scale) @ dx[..., :, None]
+                )
+            )
+        except:
+            print(
+                "SVD failed to converge. Mask could not be updated. Skipping reflection."
+            )
+            self.success = False
+            return
+
+        self.fg_mask = Z <= fg_cutoff
+        self.bg_mask = Z >= bg_cutoff
+
+        # There must be _some_ pixels in each group otherwise numerical issues happen
+        minpix = np.round(len(self.counts) * self.minfrac).astype("int")
+        zorder = np.argsort(Z)
+
+        try:
+            self.fg_mask[zorder[:minpix]] = True
+            self.bg_mask[zorder[:minpix]] = False
+            self.bg_mask[zorder[-minpix:]] = True
+            self.fg_mask[zorder[-minpix:]] = False
+        except:
+            print("Spot has 1 or fewer pixels.")
+            self.success = False
+            return
+
+    def update_background_plane(self, alpha=0.9):
+        y = self.counts
+        X = np.pad(self.difference_vectors, [[0, 0], [0, 1]], constant_values=1.0)
+        weights = np.reciprocal(self.counts)  # Inverse variance poisson because #stats
+        weights = weights * (self.bg_mask).astype("float")
+        weights = weights / weights.mean()
+
+        beta = (
+            np.linalg.pinv(X.T @ np.diag(weights) @ X) @ X.T @ (y * weights)
+        )  # if desperate, break glass
+        slope = beta[:2]
+        intercept = beta[2]
+        self.slope = (1.0 - alpha) * self.slope + alpha * slope
+        self.intercept = (1.0 - alpha) * self.intercept + alpha * intercept
+
+    def update_profile(self, alpha=0.9):
+        weights = self.counts - self.background
+        weights = np.maximum(
+            weights, self.eps
+        )  # These can be negative so we need to make them > zero(ish)
+        weights = weights * self.fg_mask
+        weights = weights / weights.mean()
+
+        scale = np.cov(self.difference_vectors.T, aweights=weights)
+
+        cen_x = np.average(self.x, weights=weights)
+        cen_y = np.average(self.y, weights=weights)
+
+        self.scale = (1.0 - alpha) * self.scale + alpha * scale
+        self.cen_x = (1.0 - alpha) * self.cen_x + alpha * cen_x
+        self.cen_y = (1.0 - alpha) * self.cen_y + alpha * cen_y
+
+    def integrate(self):
+        bg = self.background
+        var = bg + self.counts
+
+        self.I = ((self.counts - bg) * self.fg_mask).sum()
+        self.SigI = np.sqrt(((self.counts + bg) * self.fg_mask).sum())
+
+
+class SegmentedImage:
+    def __init__(self, pixels, centroids, radius=20):
+        super().__init__()
+        self.radius = radius
+        pixels = np.array(pixels).astype("float32")
+
+        x, y = np.indices(pixels.shape)
+        xy = np.dstack(np.indices(pixels.shape)).astype("float32")[:, :, ::-1]
+        k = KDTree(centroids)
+        distance, labels = k.query(xy)
+        distance = distance.astype("float32")
+        distance_vectors = centroids[labels] - xy
+        mask = (distance <= radius) & (pixels > 0)
+
+        self.data = pd.DataFrame(
+            {
+                "x": xy[mask][:, 0].astype("int"),
+                "y": xy[mask][:, 1].astype("int"),
+                "counts": pixels[mask],
+                "label": labels[mask],
+                "distance": distance[mask],
+                "dx": distance_vectors[mask][:, 0],
+                "dy": distance_vectors[mask][:, 1],
+                "cen_x": centroids[labels[mask], 0],
+                "cen_y": centroids[labels[mask], 1],
+            }
+        )
+        self.profiles = self.data.groupby("label").apply(Profile.from_dataframe)
+        [p.fit() for p in self.profiles]
+        [p.integrate() for p in self.profiles]
+        self.scale = np.stack([p.scale for p in self.profiles])
+        self.loc = np.stack([(p.cen_x, p.cen_y) for p in self.profiles])
+
+        g = self.data.groupby("label")
+        idx = np.unique(self.data.label)
+        idy = np.arange(len(centroids))
+        idx = np.isin(idy, idx)
+        self.used_reflections = idx
+        self.bboxes = np.column_stack(
+            [
+                g.min().x,
+                g.max().x + 1,
+                g.min().y,
+                g.max().y + 1,
+            ]
+        )
+        self.pixels = pixels
+        self.centroids = centroids[np.unique(self.data.label)]
+        self.labels = labels
+        self.distance = distance
+        self.distance_vectors = distance_vectors
+
+    def integrate(self, isigi_cutoff=2.0, knn=5):
+        # Implement k nearest-neighbors for weak spots to average profile params from nearby strong spots
+        strong_idx = np.array(
+            [i for i, p in enumerate(self.profiles) if p.I / p.SigI > isigi_cutoff]
+        )
+        strong_centroids = self.centroids[strong_idx]
+        for i, p in enumerate(self.profiles):
+            scale = np.zeros_like(p.scale)
+            slope = np.zeros_like(p.slope)
+            intercept = np.zeros_like(p.intercept)
+            cen_x = np.zeros_like(p.cen_x)
+            cen_y = np.zeros_like(p.cen_y)
+
+            # Average over the knn profiles
+            for idx in strong_idx[
+                np.argsort(
+                    np.linalg.norm(strong_centroids - self.centroids[i], axis=-1)
+                )
+            ][:knn]:
+                nearest_neighbor = self.profiles.iloc[idx]
+                scale += nearest_neighbor.scale / knn
+                slope += nearest_neighbor.slope / knn
+                intercept += nearest_neighbor.intercept / knn
+                cen_x += nearest_neighbor.cen_x / knn
+                cen_y += nearest_neighbor.cen_y / knn
+
+            p.set_profile_params(
+                scale,
+                slope,
+                intercept,
+                cen_x,
+                cen_y,
+            )
+        self.scale = np.stack([p.scale for p in self.profiles])
+        self.loc = np.stack([(p.cen_x, p.cen_y) for p in self.profiles])
+        return strong_idx
--- src/laue_dials/command_line/integrate.py
+++ src/laue_dials/command_line/integrate.py
@@ -1,14 +1,195 @@
 #!/usr/bin/env python
 """
-SHORT_DESCRIPTION
+This script generates integrated MTZ files from refined data with predictions
+"""
+import logging
 
-Examples
---------
+import libtbx.phil
+from dials.util import log, show_mail_handle_errors
+from dials.util.options import ArgumentParser, reflections_and_experiments_from_files
+from dials.array_family.flex import reflection_table
+from dials.array_family import flex
+from cctbx import sgtbx
 
-Usage Details
--------------
+from laue_dials.algorithms.integration import Profile, SegmentedImage
+
+import numpy as np
+import reciprocalspaceship as rs
+import gemmi
+from functools import partial
+from multiprocessing import Pool
+from itertools import repeat
+
+logger = logging.getLogger("laue-dials.command_line.integrate")
+
+help_message = """
+
+This program takes a refined geometry experiment file along with a predicted
+reflection table, and uses those to integrate intensities in the data set.
+
+The output is a MTZ file containing integrated intensities suitable for
+merging and scaling.
+
+Examples::
+
+    laue.integrate [options] poly_refined.expt predicted.refl
 """
 
+# Set the phil scope
+phil_scope = libtbx.phil.parse(
+    """
+output {
+  filename = 'integrated.mtz'
+    .type = str
+    .help = "The output MTZ filename."
 
-def integrate_dataset(expts, refls, preds, method="variable_elliptical"):
-    print("To be implemented")
+  log = 'laue.integrate.log'
+    .type = str
+    .help = "The log filename."
+  }
+
+n_proc = 8
+""",
+    process_includes=True,
+)
+
+working_phil = phil_scope.fetch(sources=[phil_scope])
+
+
+def get_refls_image(refls, img_id):
+    """
+    A function for getting the set of reflections lying on a particular image
+    """
+    return refls.select(refls["id"] == img_id)
+
+
+def integrate_image(params, img_set, refls):
+    """
+    A function for integrating predicted spots on an image
+    """
+    isigi_cutoff = 2.0  # i/sigma cutoff for strong spot profiles
+
+    # Make SegmentedImage
+    all_spots = refls["xyzcal.px"].as_numpy_array()[:, :2].astype("float32")
+    pixels = img_set.get_raw_data(0)[0].as_numpy_array().astype("float32")
+    sim = SegmentedImage(pixels, all_spots)
+
+    # Get integrated reflections only
+    refls = refls.select(flex.bool(sim.used_reflections))
+
+    # Integrate reflections
+    sim.integrate(isigi_cutoff)
+
+    # Update reflection data
+    i = np.zeros(len(refls))
+    sigi = np.zeros(len(refls))
+    bg = np.zeros(len(refls))
+    sigbg = np.zeros(len(refls))
+    profiles = sim.profiles.to_list()
+    for j in range(len(refls)):
+        prof = profiles[j]
+        if prof.success:
+            i[j] = prof.I
+            sigi[j] = prof.SigI
+            bg[j] = np.maximum((prof.background * prof.bg_mask), 0.0).sum()
+            sigbg[j] = np.sqrt(np.maximum((prof.background * prof.bg_mask), 0.0)).sum()
+    refls["intensity.sum.value"] = flex.double(i)
+    refls["intensity.sum.variance"] = flex.double(sigi**2)
+    refls["background.sum.value"] = flex.double(bg)
+    refls["background.sum.variance"] = flex.double(sigbg**2)
+    refls = refls.select(refls["intensity.sum.value"] != 0)
+    return refls  # Updated reflection table
+
+
+@show_mail_handle_errors()
+def run(args=None, *, phil=working_phil):
+    # Parse arguments
+    usage = "laue.integrate [options] poly_refined.expt predicted.refl"
+
+    parser = ArgumentParser(
+        usage=usage,
+        phil=phil,
+        read_reflections=True,
+        read_experiments=True,
+        check_format=True,
+        epilog=help_message,
+    )
+
+    params, options = parser.parse_args(args=args, show_diff_phil=True)
+
+    # Configure logging
+    log.config(verbosity=options.verbose, logfile=params.output.log)
+
+    # Log diff phil
+    diff_phil = parser.diff_phil.as_str()
+    if diff_phil != "":
+        logger.info("The following parameters have been modified:\n")
+        logger.info(diff_phil)
+
+    # Load data
+    reflections, expts = reflections_and_experiments_from_files(
+        params.input.reflections, params.input.experiments
+    )
+    preds = reflections[0]  # Get predictions
+
+    # Sanity checks
+    if len(expts) == 0:
+        parser.print_help()
+        return
+
+    # Get reflections and image data
+    imagesets = expts.imagesets()
+    ids = list(np.unique(preds["id"]).astype(np.int32))
+    get_refls = partial(get_refls_image, preds)
+    tables = list(map(get_refls, ids))
+    inputs = list(zip(repeat(params), imagesets, tables))
+
+    # Multiprocess integration
+    num_processes = params.n_proc
+    print("Starting integration")
+    with Pool(processes=num_processes) as pool:
+        refls_arr = pool.starmap(integrate_image, inputs)
+    print("Integration finished.")
+
+    # Construct an integrated reflection table
+    final_refls = flex.reflection_table()
+    for refls in refls_arr:
+        final_refls.extend(refls)
+    refls = final_refls
+
+    # Get data needed for MTZ file
+    hkl = refls["miller_index"].as_vec3_double()
+    cell = np.zeros(6)
+    for crystal in expts.crystals():
+        cell += np.array(crystal.get_unit_cell().parameters()) / len(expts.crystals())
+    cell = gemmi.UnitCell(*cell)
+    sginfo = expts.crystals()[0].get_space_group().info()
+    symbol = sgtbx.space_group_symbols(sginfo.symbol_and_number().split("(")[0])
+    spacegroup = gemmi.SpaceGroup(symbol.universal_hermann_mauguin())
+
+    # Generate rs.DataSet to write to MTZ
+    data = rs.DataSet(
+        {
+            "H": hkl.as_numpy_array()[:, 0].astype(np.int32),
+            "K": hkl.as_numpy_array()[:, 1].astype(np.int32),
+            "L": hkl.as_numpy_array()[:, 2].astype(np.int32),
+            "BATCH": refls["imageset_id"].as_numpy_array() + 1,
+            "I": refls["intensity.sum.value"].as_numpy_array(),
+            "SIGI": refls["intensity.sum.variance"].as_numpy_array() ** 0.5,
+            "xcal": refls["xyzcal.px"].as_numpy_array()[:, 0],
+            "ycal": refls["xyzcal.px"].as_numpy_array()[:, 1],
+            "wavelength": refls["wavelength"].as_numpy_array(),
+            "BG": refls["background.sum.value"].as_numpy_array(),
+            "SIGBG": refls["background.sum.variance"].as_numpy_array() ** 0.5,
+        },
+        cell=cell,
+        spacegroup=spacegroup,
+    ).infer_mtz_dtypes()
+
+    # Save reflections
+    logger.info("Saving integrated reflections to %s", params.output.filename)
+    data.write_mtz(params.output.filename, skip_problem_mtztypes=True)
+
+
+if __name__ == "__main__":
+    run()
