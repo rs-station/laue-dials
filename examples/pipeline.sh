77c62bb8672c523b5e2fc525c9a6dce5a42ae87c
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-09-26 16:12:41 -0400

    Fixed num_processors variable

--- examples/pipeline.sh
+++ examples/pipeline.sh
@@ -8,6 +8,7 @@ dials.import geometry.scan.oscillation=0,1 \
     geometry.goniometer.axes=0,1,0 \
     geometry.beam.wavelength=1.04 \
     geometry.detector.panel.pixel_size=0.08854,0.08854 \
+    lookup.mask="pixels.mask" \
     input.template=$FILE_INPUT_TEMPLATE \
 
 # Get a monochromatic geometry model
@@ -18,9 +19,9 @@ laue.sequence_to_stills monochromatic.*
 
 # Polychromatic portion
 N=12 # Max multiprocessing
-laue.optimize_indexing stills.* output.experiments="optimized.expt" output.reflections="optimized.refl" log="laue.optimize_indexing.log" n_proc=N
-laue.refine optimized.* output.experiments="poly_refined.expt" output.reflections="poly_refined.refl" output.log="laue.poly_refined.log" n_proc=N
-laue.predict poly_refined.* output.reflections="predicted.refl" output.log="laue.predict.log" n_proc=N
-laue.integrate poly_refined.expt predicted.refl output.filename="integrated.mtz" output.log="laue.integrate.log" n_proc=N
+laue.optimize_indexing stills.* output.experiments="optimized.expt" output.reflections="optimized.refl" log="laue.optimize_indexing.log" wavelengths.lam_min=0.95 wavelengths.lam_max=1.15 n_proc=$N
+laue.refine optimized.* output.experiments="poly_refined.expt" output.reflections="poly_refined.refl" output.log="laue.poly_refined.log" n_proc=$N
+laue.predict poly_refined.* output.reflections="predicted.refl" output.log="laue.predict.log" n_proc=$N
+laue.integrate poly_refined.expt predicted.refl output.filename="integrated.mtz" output.log="laue.integrate.log" n_proc=$N
 
 # This is where laue_dials ends. The output file total_integrated.mtz can be merged in careless and refined in phenix to get a model
