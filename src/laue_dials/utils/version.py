b11ac43915db4d5f7aad04d7722f0a8307e1abf4
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-12-13 15:16:36 -0500

    Added Google-style docstrings for all util functions

--- src/laue_dials/utils/version.py
+++ src/laue_dials/utils/version.py
@@ -2,11 +2,22 @@
 import os
 from importlib.metadata import version
 
-
 def laue_version():
+    """
+    Print the versions of DIALS and laue-dials packages.
+
+    This function retrieves the versions of the DIALS and laue-dials packages and prints
+    them to the terminal. 
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     try:
         width = os.get_terminal_size().columns
-    except:
+    except OSError:
         width = 65
     dials_version = version("dials")
     laue_dials_version = version("laue-dials")
