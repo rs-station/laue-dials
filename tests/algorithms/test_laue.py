cb02810c49368a7eb79b9f2e2398ff6d7be49e88
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2024-01-05 16:26:44 -0500

    Additional tests for algorithms

--- tests/algorithms/test_laue.py
+++ tests/algorithms/test_laue.py
@@ -1,86 +1,71 @@
-import unittest
-
-import gemmi
 import numpy as np
-import reciprocalspaceship as rs
-
-from laue_dials.algorithms.laue import *
-
-
-class TestLaueBase(unittest.TestCase):
-    def setUp(self):
-        # Set up a test instance of LaueBase
-        self.s0 = np.array([1, 0, 0])
-        self.cell = gemmi.UnitCell(50, 50, 50, 90, 90, 90)
-        self.R = np.eye(3)
-        self.lam_min = 0.5
-        self.lam_max = 1.5
-        self.dmin = 2
-        self.spacegroup = "P1"
-        self.LaueBase_obj = LaueBase(
-            self.s0,
-            self.cell,
-            self.R,
-            self.lam_min,
-            self.lam_max,
-            self.dmin,
-            self.spacegroup,
-        )
-
-    def test_cell(self):
-        self.assertEqual(self.LaueBase_obj.cell.a, 50)
-        self.assertEqual(self.LaueBase_obj.cell.b, 50)
-        self.assertEqual(self.LaueBase_obj.cell.c, 50)
-        self.assertEqual(self.LaueBase_obj.cell.alpha, 90)
-        self.assertEqual(self.LaueBase_obj.cell.beta, 90)
-        self.assertEqual(self.LaueBase_obj.cell.gamma, 90)
-
-    def test_spacegroup(self):
-        self.assertEqual(self.LaueBase_obj.spacegroup.number, 1)
-
-    def test_RB(self):
-        RB_expected = np.array(self.LaueBase_obj.cell.fractionalization_matrix).T
-        np.testing.assert_array_almost_equal(
-            self.LaueBase_obj.RB, RB_expected, decimal=3
-        )
-
-    def test_s0(self):
-        s0_expected = np.array([1, 0, 0])
-        np.testing.assert_array_almost_equal(
-            self.LaueBase_obj.s0, s0_expected, decimal=3
-        )
-
-    def test_Hall(self):
-        # Check that all reflections have a d-value above the minimum
-        d = self.LaueBase_obj.cell.calculate_d_array(self.LaueBase_obj.Hall)
-        self.assertTrue(np.all(d >= self.dmin))
-
-        # Check that all systematic absences are removed
-        self.assertTrue(
-            np.all(
-                ~rs.utils.is_absent(
-                    self.LaueBase_obj.Hall, self.LaueBase_obj.spacegroup
-                )
-            )
-        )
+import pytest
+from dxtbx.model import ExperimentList
+from dials.array_family.flex import reflection_table
+from laue_dials.algorithms.laue import (
+    store_wavelengths,
+    remove_beam_models,
+    gen_beam_models,
+    LaueBase,
+    LaueAssigner,
+    LauePredictor,
+)
 
 
-class TestLaueAssigner(unittest.TestCase):
-    def setUp(self):
-        # Set up a test instance of LaueAssigner
-        self.s0 = np.array([1, 0, 0])
-        self.cell = gemmi.UnitCell(50, 50, 50, 90, 90, 90)
-        self.R = np.eye(3)
-        self.lam_min = 0.5
-        self.lam_max = 1.5
-        self.dmin = 2
-        self.spacegroup = "P1"
-        self.laue_assigner = LaueAssigner(
-            self.s0,
-            self.cell,
-            self.R,
-            self.lam_min,
-            self.lam_max,
-            self.dmin,
-            self.spacegroup,
-        )
+@pytest.fixture
+def example_experiments_reflections():
+    expts = ExperimentList.from_file("../data/optimized.expt", check_format=False)
+    refls = reflection_table.from_file("../data/optimized.refl")
+    return expts, refls
+
+
+def test_gen_beam_models(example_experiments_reflections):
+    expts, refls = example_experiments_reflections
+    new_expts, new_refls = gen_beam_models(expts, refls)
+    assert len(new_expts) == len(new_refls)
+
+
+def test_remove_beam_models(example_experiments_reflections):
+    expts, refls = example_experiments_reflections
+    original_ids = refls["id"]
+    new_expts, new_refls = gen_beam_models(expts, refls)
+    final_expts = remove_beam_models(new_expts, original_ids[0])
+    assert len(final_expts) == len(expts.imagesets())
+
+
+def test_store_wavelengths(example_experiments_reflections):
+    expts, refls = example_experiments_reflections
+    new_expts, new_refls = gen_beam_models(expts, refls)
+    final_refls = store_wavelengths(expts, refls)
+    assert "wavelength" in final_refls.keys()
+
+
+def test_LaueBase():
+    s0 = np.array([0, 0, 1])
+    cell = (1, 1, 1, 90, 90, 90)
+    R = np.eye(3)
+    lam_min, lam_max, dmin = 0.8, 1.2, 1.0
+    spacegroup = "P1"
+    laue_base = LaueBase(s0, cell, R, lam_min, lam_max, dmin, spacegroup)
+    assert laue_base is not None
+
+
+def test_LaueAssigner():
+    s0 = np.array([0, 0, 1])
+    s1 = np.array([[1, 1, 1], [2, 2, 2]])
+    cell = (1, 1, 1, 90, 90, 90)
+    R = np.eye(3)
+    lam_min, lam_max, dmin = 0.8, 1.2, 1.0
+    spacegroup = "P1"
+    laue_assigner = LaueAssigner(s0, s1, cell, R, lam_min, lam_max, dmin, spacegroup)
+    assert laue_assigner is not None
+
+
+def test_LauePredictor():
+    s0 = np.array([0, 0, 1])
+    cell = (1, 1, 1, 90, 90, 90)
+    R = np.eye(3)
+    lam_min, lam_max, dmin = 0.8, 1.2, 1.0
+    spacegroup = "P1"
+    laue_predictor = LauePredictor(s0, cell, R, lam_min, lam_max, dmin, spacegroup)
+    assert laue_predictor is not None
