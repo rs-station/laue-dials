dceb4f8642adcacd27d71db7791d9e4402528a74
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2024-01-16 15:39:27 -0500

    Added integration algorithm documentation

--- src/laue_dials/command_line/integrate.py
+++ src/laue_dials/command_line/integrate.py
@@ -17,8 +17,7 @@ import reciprocalspaceship as rs
 from cctbx import sgtbx
 from dials.array_family import flex
 from dials.util import show_mail_handle_errors
-from dials.util.options import (ArgumentParser,
-                                reflections_and_experiments_from_files)
+from dials.util.options import ArgumentParser, reflections_and_experiments_from_files
 
 from laue_dials.algorithms.integration import SegmentedImage
 from laue_dials.utils.version import laue_version
@@ -35,6 +34,13 @@ The program takes a refined geometry experiment file along with a predicted
 reflection table, and uses those to integrate intensities in the data set.
 The output is an MTZ file containing integrated intensities suitable for
 merging and scaling.
+
+The algorithm applied here is a variable elliptical summation algorithm
+inspired by the VariableElliptical mode in Precognition. Elliptical
+profiles are modeled for each strong reflection, with weak reflections
+using an average of the k nearest strong spots, and then the pixel
+intensities within are summed to generate integrated intensities
+per reflection.
 
 Examples:
 
