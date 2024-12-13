c9a752174a5a78cf475cbcae01b8f02f201d558a
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2024-12-13 13:46:21 -0500

    Added laue.version command source code

---
+++ src/laue_dials/command_line/version.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+This script performs spotfinding on an imported experiment.
+"""
+
+from laue_dials.utils.version import laue_version
+
+help_message = """
+Prints version information for Laue-DIALS and DIALS.
+
+Examples:
+
+    laue.version
+"""
+
+def run():
+    """
+    Print version information to the terminal and logfile.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Print version information
+    print(laue_version())
+
+if __name__ == "__main__":
+    run()
