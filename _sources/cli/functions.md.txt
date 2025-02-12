1853d7ed5cee6791d13c79acb4069c7e44650fe6
GitHub Action <action@github.com>
2025-02-12 16:19:32 +0000

    Upload documentation from latest commit

--- _sources/cli/functions.md.txt
+++ _sources/cli/functions.md.txt
@@ -6,6 +6,7 @@
 .. toctree::
    :maxdepth: 1
 
+   version
    find_spots
    index
    sequence_to_stills
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g9871cd0',
+    VERSION: '0.1.dev1+gf00c7f0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
