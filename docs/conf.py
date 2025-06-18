d1e59d2d264ce643dd80829daf9398e383d0ac55
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2025-06-18 11:54:43 -0400

    Ignore Jupyter ipynb files for building docs

--- docs/conf.py
+++ docs/conf.py
@@ -129,7 +129,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", "tutorials/*/*.ipynb"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
